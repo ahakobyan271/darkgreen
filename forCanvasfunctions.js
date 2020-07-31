daef07f62fe43d1b96ea50cdb9081b056d995488
ahakobyan271 <63904819+ahakobyan271@users.noreply.github.com>
2020-07-31 14:12:09 +0400

    s

---
+++ forCanvasfunctions.js
@@ -0,0 +1,434 @@
+var canvas = new fabric.Canvas('c10', { width: 920, height: 850, top: 60, backgroundColor: "rgb(4, 78, 72)" });
+
+
+
+
+
+canvas.on('mouse:wheel', function (opt) {  //desktop zoom in out
+    var delta = opt.e.deltaY;
+    var zoom = canvas.getZoom();
+    zoom *= 0.999 ** delta;
+    if (zoom > 0.5) zoom = 0.5;
+    if (zoom < 0.3) zoom = 0.3;
+    canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+    opt.e.preventDefault();
+    opt.e.stopPropagation();
+});
+
+
+
+
+
+
+
+/*window.addEventListener('load', function () {
+
+  const loader = document.querySelector('.loader')
+
+  loader.classList.add('hidden')
+})*/
+
+
+
+//alert are you sure you want to leave?
+//var back = false;
+//back = true; //Somewhere, the condition is set to true
+window.onbeforeunload = function (e) {
+    //if (back == true) 
+    calctotal()
+
+    // return "Are you sure to exit?";
+}
+
+
+
+
+window.onload = function (e) {
+    //console.log(canvas.getObjects())
+    calctotal()
+
+}
+
+
+
+function saveimage() {
+    var link = canvas.toDataURL("image/png") //.replace("image/png", "image/octet-stream");
+    //console.log(link)
+    //document.getElementById('linkimg').src = link
+    localStorage.setItem('img', link)
+    //window.location = link
+
+    //saveAs(document.getElementById('linkimg').src) //download png file userside
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function downloadimage() {
+
+
+    fabric.Image.fromURL(src = "image/watermark.png", img => {
+
+        img.scale(1.5).set({ top: 150, left: 230, price: 0, name: "watermark" });
+        canvas.add(img);
+
+
+
+
+        savewithwatermark()
+    })
+
+    function savewithwatermark() {
+        var link = canvas.toDataURL("image/png")
+
+
+        saveAs(link) //download png file userside
+        window.location.reload()
+
+    }
+}
+
+
+
+
+
+//save last design 
+/*function getimgls() {
+    var retrieved = localStorage.getItem('img')
+    //console.log(retrieved)
+    document.getElementById('linkimg').src = retrieved
+
+
+}
+
+getimgls()*/
+
+
+
+
+
+var deleteIcon = "data:image/svg+xml,%3C%3Fxml version='1.0' encoding='utf-8'%3F%3E%3C!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'%3E%3Csvg version='1.1' id='Ebene_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='595.275px' height='595.275px' viewBox='200 215 230 470' xml:space='preserve'%3E%3Csquare style='fill:%23F44336;' cx='299.76' cy='439.067' r='218.516'/%3E%3Cg%3E%3Crect x='267.162' y='307.978' transform='matrix(0.7071 -0.7071 0.7071 0.7071 -222.6202 340.6915)' style='fill:red;' width='65.545' height='262.18'/%3E%3Crect x='266.988' y='308.153' transform='matrix(0.7071 0.7071 -0.7071 0.7071 398.3889 -83.3116)' style='fill:red;' width='65.544' height='262.179'/%3E%3C/g%3E%3C/svg%3E";
+
+
+
+
+
+
+var img = document.createElement('img');
+img.src = deleteIcon;
+/*controls*/
+
+fabric.Object.prototype.transparentCorners = false;
+
+//fabric.Object.prototype.cornerColor = 'grey';
+fabric.Object.prototype.cornerSize = 15;
+fabric.Object.prototype.cornerColor = 'white';
+
+fabric.Object.prototype.cornerStyle = 'circle';
+fabric.Object.prototype.hasControls = true;
+fabric.Object.prototype.hasBorders = true;
+fabric.Object.prototype.borderColor = 'white';
+fabric.Object.prototype.borderSize = 6;
+
+canvas.defaultCursor = "pointer";
+canvas.selection = true;
+
+canvas.on('selection:created', function (ev) {
+    ev.target.set({
+        lockScalingX: true,                      /*lockscaling of selected group*/
+        lockScalingY: true
+    });
+});
+
+canvas.on('object:added', function (ev) {
+    ev.target.scale(0.9).set({
+        top: 150, left: 230,
+        lockScalingX: true,                     /*lockscaling of single item*/
+        lockScalingY: true
+
+    });
+    console.log(ev.target.price)
+
+
+});
+
+/*delete  icon*/
+
+
+fabric.Object.prototype.controls.deleteControl = new fabric.Control({
+    position: { x: 0.45, y: -0.45 },
+    offsetY: 16,
+    cursorStyle: 'pointer',
+    mouseUpHandler: deleteObject,
+    render: renderIcon,
+    cornerSize: 44
+});
+
+
+
+function deleteObject(eventData, target) {
+    var canvas = target.canvas;
+    /* target.price=0;*/
+    canvas.remove(target);
+    canvas.requestRenderAll();
+}
+
+function renderIcon(ctx, left, top, styleOverride, fabricObject) {
+    var size = this.cornerSize;
+    ctx.save();
+    ctx.translate(left, top);
+    ctx.rotate(fabric.util.degreesToRadians(fabricObject.angle));
+    ctx.drawImage(img, -size / 2, -size / 2, size, size);
+    ctx.restore();
+}
+
+/*delete*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*libraries   to show or hide eache library  */
+
+/*var library1 = document.getElementById('library1')
+var button1 = document.getElementById('detail option1')
+button1.addEventListener('click', function () {
+
+    if (library1.className === 'hide') {
+        library1.classList.add("showing")
+        button1.innerHTML = "Hide details"
+        /* $(function () {
+           $(library1).siblings().removeClass('showing');
+         });
+
+    } else {
+        library1.className = "hide";
+        button1.innerHTML = "Show details"
+    }
+})*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*click unclicked  button function  for buttons*/
+
+
+
+$(function () {
+    $('.unclicked').on('click', function () {
+        $(this).toggleClass('clicked');
+        $(this).siblings().removeClass('clicked');
+
+
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* clear all from canvas function*/
+
+
+const totalPrice = document.querySelector('.total')
+
+const clearButton = document.getElementById('detail option9')
+
+clearButton.addEventListener('click', clearcanvas)
+
+function clearcanvas() {
+    canvas.clear()
+    calctotal()
+    //console.log(canvas.getObjects())
+
+}
+
+
+
+
+/*calculate total price of added objects*/
+
+
+
+canvas.on('object:added', addobject)
+function addobject() {
+    console.log(canvas.getObjects())
+    calctotal()
+
+}
+
+
+function calctotal() {
+    var obj = canvas.getObjects()
+    var total = 0
+    var idner = 0
+    for (let i = 0; i < obj.length; i++) {
+
+
+
+        total += obj[i].price
+
+
+
+        idner += "id" + obj[i].id
+
+
+
+
+    }
+
+
+    console.log(total)
+    if (obj.length > 3 && obj.length <= 6 && total > 30) {
+
+        //console.log(total);
+
+
+        total -= (total * 0.1);
+
+
+    }
+    else if (obj.length > 7 && obj.length <= 10) {
+        //console.log(obj.length);
+
+
+        total -= (total * 0.2);
+
+
+    }
+
+    else if (obj.length > 10) {
+        //console.log(obj.length);
+
+
+        total -= (total * 0.25);
+
+
+    }
+
+
+
+
+    totalPrice.innerHTML = "Total:" + "$" + total
+
+    localStorage.setItem("total price", total)
+    localStorage.setItem("idner", idner)
+
+
+    //console.log(obj.price)
+}
+
+
+canvas.on('object:removed', function () {
+    console.log(canvas.getObjects())
+    canvas.requestRenderAll()
+    calctotal()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ newjson.js
@@ -0,0 +1,743 @@
+
+
+
+
+
+
+
+
+var cart = {}
+
+
+$('document').ready(function () {
+    loadgoods()
+    checkcart()
+    displayStyles(styles) //display style options
+
+
+
+});
+var general = []
+var styles = []
+//var matches=[]
+
+
+
+
+function loadgoods() {
+
+
+    $.getJSON('data.json', function (data) {
+
+
+        //console.log(Object.keys(data).length)
+        for (var key in data) {
+            general.push(data[key])
+        }
+        //console.log(general[200])
+
+
+
+
+        document.querySelectorAll('.cellforicon').forEach(el => {  //event listener for each style option
+            el.addEventListener("click", srchfilter)
+
+            function srchfilter(e) {
+
+                // console.log(general)
+
+                //console.log(e.target)
+                var cell = e.target.parentElement
+                var styleindicator = cell.querySelectorAll('#styleindicator')[0].innerHTML
+                var searchtext = document.getElementById('searchbar')
+                searchtext.innerHTML = "Details: " + styleindicator
+                var searchtextfilter = el.dataset.search.toLowerCase()
+                var matches = general.filter((key) => {
+
+                    return (key.name.toLowerCase().trim().includes(searchtextfilter))
+
+
+                })
+                // console.log(matches.length)
+                document.getElementById('results').innerHTML = "Search results: " + matches.length
+                display(matches)
+                // checksearchtext()
+
+                // document.getElementById('search').addEventListener('input', checksearchtext)
+                /* function checksearchtext() {
+                     var searchtext = document.getElementById('search')
+ 
+                     if (searchtext.value === 'All') {
+                         matches = []
+ 
+                         //console.log(matches)
+ 
+                         display(reversedGeneral)
+                         document.getElementById('results').innerHTML = "Total number of details: " + reversedGeneral.length
+ 
+ 
+                     } }*/
+
+
+
+            }
+
+        }
+        )
+
+
+
+
+
+
+
+        const display = (general) => { //display details by styles
+
+            document.getElementById("general1").innerHTML = `
+        ${general.map(function (search) {
+
+                return `<div class="cell" data-art=${search.id}  >
+            <img data-price=${search.price} class="detail img "   src="${search.photo}"   >
+            
+            
+            
+            <p class="genarray"  >${search.name}</p>
+            <p class="genarray"  >${search.material}</p>
+            <p class="genarray" >$${search.weight * 3}</p>
+           
+            <p  class="addtodesignBtn ">Add to my design</p>
+            
+    
+            
+        </div>`
+            }).join("")
+
+                }`
+            addtocart()
+            showbutton()
+        }
+        var reversedGeneral = general.reverse()
+        //display(reversedGeneral)
+        console.log(reversedGeneral.length)
+        document.getElementById('results').innerHTML = "Total number of details: " + reversedGeneral.length
+
+
+
+
+        /*const display=matches=>{
+            var out=''
+            for(var key in matches){
+                console.log(key)
+        out+='<div class="cell">';
+        out+='<img data-price='+matches[key]['price']+' class="detail img "  src="'+matches[key]['photo']+'"   >';
+        out+='<p class="price"> '+matches[key]['name']+'</p>'
+        out+='<p class="price">$'+matches[key]['price']+'</p>'
+        
+        out+='<p class="count"> 0 </p>'
+        out+='<button data-art="'+key+'">add to</button>'
+        out+='</div>';
+        
+            }
+        
+            $('#general1').html(out);
+        }*/
+        // display(general)
+
+
+    })
+}
+
+
+
+
+
+
+//filter  for search
+
+
+var search = document.getElementById('search')
+
+
+
+//console.log(general)
+
+
+function addtocart() {  //addtocart , second part is in cart js
+    document.querySelectorAll('.cell').forEach(el => {
+        el.addEventListener("click", addimg)
+
+        function addimg(e) {
+            var articul = el.dataset.art
+            cart[articul] = 1
+            //console.log(e.target)
+            var cell = e.target.parentElement
+            var button = cell.querySelectorAll('.addtodesignBtn')[0]
+
+            e.target.parentElement.classList.add('cellclicked')
+            button.innerHTML = "Added"
+            button.classList.add('addtodesignBtnclicked')
+
+            localStorage.setItem('cart', JSON.stringify(cart))
+
+            // console.log(cart)
+        }
+
+    })
+
+}
+
+
+
+
+
+function checkcart() {
+
+    if (localStorage.getItem('cart') != null) {
+
+        cart = JSON.parse(localStorage.getItem('cart'))
+
+    }
+
+}
+
+
+
+
+/*var lists = document.querySelectorAll('li') //hide the list of details
+function hide() {
+
+    for (i = 0; i < lists.length; i++)
+
+        lists[i].classList.add('hidden')
+
+
+}
+
+hide()
+
+
+
+
+
+document.querySelector('.fa').addEventListener('click', function (e) { //show the list of details by click
+
+
+    // console.log(e.target)
+
+    for (i = 0; i < lists.length; i++)
+
+        lists[i].classList.toggle('hidden')
+
+})*/
+
+
+
+
+var styles = [
+    {
+        name: "Sea",
+        searchname: "sea",
+
+        photo: "image/seaicon.png"
+
+    },
+    {
+        name: "Letters",
+        searchname: "letters",
+
+        photo: "image/lettericon.png"
+
+    },
+
+    {
+        name: "Butterflies",
+        searchname: "butterflies",
+
+        photo: "image/butterflyicon.png"
+
+    },
+
+    {
+        name: "Pearls",
+        searchname: "pearls",
+
+        photo: "image/pearlicon.png"
+
+    },
+
+
+
+    {
+        name: "Sky",
+        searchname: "sky",
+
+        photo: "image/skyicon.png"
+
+    },
+
+
+    {
+        name: "Nature",
+        searchname: "nature",
+
+        photo: "image/flowericon.png"
+
+    },
+    {
+        name: "Hearts",
+        searchname: "hearts",
+
+        photo: "image/hearticon.png"
+
+    },
+
+    {
+        name: "Gems",
+        searchname: "gems",
+
+        photo: "image/gemicon.png"
+
+    },
+
+    {
+        name: "Mannequins",
+        searchname: "mannequin",
+
+        photo: "image/mannequinicon.png"
+
+    },
+    {
+        name: "Small size",
+        searchname: "small",
+
+        photo: "image/smalldetailicon2.png"
+
+    },
+    {
+        name: "Medium size ",
+        searchname: "medium",
+
+        photo: "image/mediumdetailicon2.png"
+
+    },
+    {
+        name: "Large size",
+        searchname: "large",
+
+        photo: "image/largedetailicon.png"
+
+    },
+    {
+        name: "Zodiac",
+        searchname: "zodiac",
+
+        photo: "image/zodiac.png"
+
+    },
+
+
+    {
+        name: "Earcuff earrings",
+        searchname: "chainearcuff",
+
+        photo: "image/earcuffchain.png"
+
+    },
+
+
+    {
+        name: "Hoops",
+        searchname: "hoop",
+
+
+        photo: "image/hoopicon.png"
+
+    },
+
+    {
+        name: "Climber earrings",
+        searchname: "climber",
+        photo: "image/climbericon.png"
+
+    },
+    {
+        name: "Stud earrings",
+        searchname: "stud",
+        photo: "image/studicon.png"
+
+    },
+    {
+        name: "Earcuffs",
+        searchname: "earcuffs",
+        photo: "image/earcufficon.png"
+
+    },
+
+    {
+        name: "Statement earrings",
+        searchname: "statementearring",
+        photo: "image/statementicon.png"
+
+    },
+    {
+        name: "Drop earrings ",
+        searchname: "drop",
+        photo: "image/dropearringicon.png"
+
+    },
+    {
+        name: "Ring bracelets",
+        searchname: "bracelet",
+        photo: "image/ringbraceleticon.png"
+
+    },
+    {
+        name: "Helix bracelets",
+        searchname: "bracelet",
+        photo: "image/helixbraceleticon.png"
+
+    },
+    {
+        name: "Bracelets",
+        searchname: "bracelet",
+        photo: "image/bracelets.png"
+
+    },
+    {
+        name: "Rings",
+        searchname: "rings",
+        photo: "image/ringsicon.png"
+
+    },
+
+    {
+        name: "Connected rings",
+        searchname: "rings",
+        photo: "image/doubleringicon.png"
+
+    },
+    {
+        name: "Necklaces",
+        searchname: "necklaces",
+        photo: "image/necklaceicon.png"
+
+    }
+
+
+
+]
+
+
+
+
+const displayStyles = (styles) => {   //display style options after window load
+
+    document.getElementById("general1").innerHTML = `
+     ${styles.map(function (search) {
+
+        return `<div class="cellforicon" data-search=${search.searchname} >
+         <img data-price=${search.price} class="detail img imgforicon "  src="${search.photo}"   >
+         
+         
+         <p class="price">Details:</p>
+         <p id="styleindicator">${search.name}</p>
+         
+         
+         
+ 
+         
+     </div>`
+    }).join("")
+
+        }`
+    hidebutton()
+}
+//console.log(styles)
+
+
+var searchtext = document.getElementById('searchbar')
+searchtext.innerHTML = "Choose style and details for your design and make compositions on Canvas"
+
+
+
+document.getElementById('stylebutton').addEventListener('click', returnstyles) //display style options after button click
+
+function returnstyles(styles) {
+    document.getElementById('results').innerHTML = "Total number of details: " + general.length
+
+    var searchtext = document.getElementById('searchbar')
+    searchtext.innerHTML = "Choose style and details for your design and make compositions on Canvas"
+
+    var styles = [
+
+        {
+            name: "Sea",
+            searchname: "sea",
+
+            photo: "image/seaicon.png"
+
+        },
+        {
+            name: "Letters",
+            searchname: "letters",
+
+            photo: "image/lettericon.png"
+
+        },
+
+        {
+            name: "Butterflies",
+            searchname: "butterflies",
+
+            photo: "image/butterflyicon.png"
+
+        },
+
+        {
+            name: "Pearls",
+            searchname: "pearls",
+
+            photo: "image/pearlicon.png"
+
+        },
+
+
+
+        {
+            name: "Sky",
+            searchname: "sky",
+
+            photo: "image/skyicon.png"
+
+        },
+
+
+        {
+            name: "Nature",
+            searchname: "nature",
+
+            photo: "image/flowericon.png"
+
+        },
+        {
+            name: "Hearts",
+            searchname: "hearts",
+
+            photo: "image/hearticon.png"
+
+        },
+
+        {
+            name: "Gems",
+            searchname: "gems",
+
+            photo: "image/gemicon.png"
+
+        },
+
+        {
+            name: "Mannequins",
+            searchname: "mannequin",
+
+            photo: "image/mannequinicon.png"
+
+        },
+        {
+            name: "Small size",
+            searchname: "small",
+
+            photo: "image/smalldetailicon2.png"
+
+        },
+        {
+            name: "Medium size ",
+            searchname: "medium",
+
+            photo: "image/mediumdetailicon2.png"
+
+        },
+        {
+            name: "Large size",
+            searchname: "large",
+
+            photo: "image/largedetailicon.png"
+
+        },
+        {
+            name: "Zodiac",
+            searchname: "zodiac",
+
+            photo: "image/zodiac.png"
+
+        },
+
+
+        {
+            name: "Earcuff earrings",
+            searchname: "chainearcuff",
+
+            photo: "image/earcuffchain.png"
+
+        },
+
+
+        {
+            name: "Hoops",
+            searchname: "hoop",
+
+
+            photo: "image/hoopicon.png"
+
+        },
+
+        {
+            name: "Climber earrings",
+            searchname: "climber",
+            photo: "image/climbericon.png"
+
+        },
+        {
+            name: "Stud earrings",
+            searchname: "stud",
+            photo: "image/studicon.png"
+
+        },
+        {
+            name: "Earcuffs",
+            searchname: "earcuffs",
+            photo: "image/earcufficon.png"
+
+        },
+
+        {
+            name: "Statement earrings",
+            searchname: "statementearring",
+            photo: "image/statementicon.png"
+
+        },
+        {
+            name: "Drop earrings ",
+            searchname: "drop",
+            photo: "image/dropearringicon.png"
+
+        },
+        {
+            name: "Ring bracelets",
+            searchname: "bracelet",
+            photo: "image/ringbraceleticon.png"
+
+        },
+        {
+            name: "Helix bracelets",
+            searchname: "bracelet",
+            photo: "image/helixbraceleticon.png"
+
+        },
+        {
+            name: "Bracelets",
+            searchname: "bracelet",
+            photo: "image/bracelets.png"
+
+        },
+        {
+            name: "Rings",
+            searchname: "rings",
+            photo: "image/ringsicon.png"
+
+        },
+
+        {
+            name: "Connected rings",
+            searchname: "rings",
+            photo: "image/doubleringicon.png"
+
+        },
+        {
+            name: "Necklaces",
+            searchname: "necklaces",
+            photo: "image/necklaceicon.png"
+
+        }
+
+
+
+    ]
+
+
+    displayStyles(styles)
+    //console.log(styles)
+    document.querySelectorAll('.cellforicon').forEach(el => {
+        el.addEventListener("click", srchfilter)
+
+        function srchfilter(e) {
+            // console.log(e.target)
+            var cell = e.target.parentElement
+            var styleindicator = cell.querySelectorAll('#styleindicator')[0].innerHTML
+            var searchtext = document.getElementById('searchbar')
+            searchtext.innerHTML = "Details: " + styleindicator
+
+            var searchtextfilter = el.dataset.search.toLowerCase()
+            var matches = general.filter((key) => {
+                return (key.name.toLowerCase().trim().includes(searchtextfilter))
+
+            })
+
+
+
+            const display = (general) => {
+
+                document.getElementById("general1").innerHTML = `
+                ${general.map(function (search) {
+
+                    return `<div class="cell" data-art=${search.id}  >
+            <img data-price=${search.price} class="detail img "   src="${search.photo}"   >
+            
+            
+            
+           
+            <p class="genarray"  >${search.name}</p>
+            <p class="genarray"  >${search.material}</p>
+            <p class="genarray" >$${search.weight * 3}</p>
+           
+            <p  class="addtodesignBtn ">Add to my design</p>
+            
+                    
+            
+                    
+                </div>`
+                }).join("")
+
+                    }`
+                addtocart()
+                showbutton()
+            }
+
+            document.getElementById('results').innerHTML = "Search results: " + matches.length
+            display(matches)
+
+
+        }
+
+
+    }
+    )
+
+}
+function hidebutton() { //hide Back button
+
+    document.getElementById('stylebutton').classList.add("hidden")
+
+}
+hidebutton()
+
+
+
+
+function showbutton() { //show Back button
+
+    document.getElementById('stylebutton').classList.remove("hidden")
+
+
+
+}
+
